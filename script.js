9339ff4e07f7013af7288b2c5e42043b522e7023
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-12 19:51:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let s= prompt();
+
+function f(s)
+{
+   for(let i=0;i<s.length;i++)
+   {
+       let flag=true;
+       let c = s.charAt(i);
+       for(let j=0;j<s.length;j++)
+       {
+           let c1=s.charAt(j)
+           if(c==c1 && i!=j)
+           {
+               flag=false;
+               break
+           }
+           
+       }
+       if(flag==true)
+       {
+           return c;
+       }
+   }
+    
+}
+let result=f(s);
+console.log(result);
